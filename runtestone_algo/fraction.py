310be22981b6412554df4e78062012b7c2b80beb
rob <thaty.poly@gmail.com>
2021-01-30 10:17:02 -0500

    vscode commit test

---
+++ runtestone_algo/fraction.py
@@ -0,0 +1,21 @@
+import math
+
+class Fraction:
+
+	def __init__(self,top,bottom):
+
+		self.num = top
+		self.den = bottom
+
+	def __str__(self):
+		return str(self.num)+"/"+str(self.den)
+
+	def __add__(self,otherfraction):
+		newnum = self.num*otherfraction.den + self.den*otherfraction.num
+		newden = self.den * otherfraction.den
+		common = math.gcd(newnum, newden)
+
+		return Fraction(newnum//common, newden//common)
+
+myf = Fraction(3,5)
+print(myf)
