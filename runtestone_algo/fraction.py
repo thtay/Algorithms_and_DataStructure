36d469de38b4cf5dd29d0bad2079a40be80628bf
rob <thaty.poly@gmail.com>
2021-01-30 11:10:06 -0500

    black format

--- runtestone_algo/fraction.py
+++ runtestone_algo/fraction.py
@@ -1,21 +1,22 @@
 import math
 
+
 class Fraction:
+    def __init__(self, top, bottom):
 
-	def __init__(self,top,bottom):
+        self.num = top
+        self.den = bottom
 
-		self.num = top
-		self.den = bottom
+    def __str__(self):
+        return str(self.num) + "/" + str(self.den)
 
-	def __str__(self):
-		return str(self.num)+"/"+str(self.den)
+    def __add__(self, otherfraction):
+        newnum = self.num * otherfraction.den + self.den * otherfraction.num
+        newden = self.den * otherfraction.den
+        common = math.gcd(newnum, newden)
 
-	def __add__(self,otherfraction):
-		newnum = self.num*otherfraction.den + self.den*otherfraction.num
-		newden = self.den * otherfraction.den
-		common = math.gcd(newnum, newden)
+        return Fraction(newnum // common, newden // common)
 
-		return Fraction(newnum//common, newden//common)
 
-myf = Fraction(3,5)
+myf = Fraction(3, 5)
 print(myf)
