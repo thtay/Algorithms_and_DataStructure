7e3e7cfcb0a9a6615b5e7e2ccb8f6c45d5cae1d3
rob <thaty.poly@gmail.com>
2022-09-11 23:00:40 -0400

    Array-minSubArrayLen

--- general-practice/tests/test_arrays.py
+++ general-practice/tests/test_arrays.py
@@ -7,7 +7,7 @@ import general_practice.arrays as arr
 
 class arraysTest(unittest.TestCase):
 
-    def test_arrays(self):
+    def test_arrays_length_of_longest_subString(self):
         """
         Case: pwwkey
         Expected: 3
@@ -16,3 +16,20 @@ class arraysTest(unittest.TestCase):
         ans = arr.lengthOfLongestSubstring(test_string)
         self.assertEquals(ans, 3)
 
+    def test_minSubArrayLen(self):
+        """
+        minSubArrayLen Tests
+        """
+        test_array_1 = [2,3,1,2,4,3]
+        test_array_1_target = 7
+        test_array_1_ans = 2
+        test_array_2 = [1,4,4]
+        test_array_2_target = 4
+        test_array_2_ans = 1
+        test_array_3 = [1,1,1,1,1,1,1,1]
+        test_array_3_target = 11
+        test_array_3_ans = 0
+
+        self.assertEquals(arr.minSubArrayLen(test_array_1_target,test_array_1), test_array_1_ans)
+        self.assertEquals(arr.minSubArrayLen(test_array_2_target,test_array_2), test_array_2_ans)
+        self.assertEquals(arr.minSubArrayLen(test_array_3_target,test_array_3), test_array_3_ans)
